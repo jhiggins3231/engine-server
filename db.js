eec0027f95b58283081c22c7bb4b8cf5c5cdbfeb
James <jhiggins3231@gmail.com>
2019-11-01 11:36:17 -0400

    first

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres' 
+const sequelize = new Sequelize(process.env.DATABASE_URL ||
+    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/nissanWishlist`,
+    {
+        dialect: 'postgres' 
 });
 
 sequelize.authenticate()
