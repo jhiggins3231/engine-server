06ec76d36532effc1afe2c6231b5d460305ca384
James <jhiggins3231@gmail.com>
2019-11-04 10:34:13 -0500

    remove special character

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL ||
-    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/nissanWishlist`,
-    {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres' 
 });
 
